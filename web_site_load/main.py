eaf8989317396f98bae4faf7ed1f3b247f4cd150
Don Bowman <don@agilicus.com>
2021-03-07 16:42:32 -0500

    fix: correct issue w/ missing entry

--- web_site_load/main.py
+++ web_site_load/main.py
@@ -8,26 +8,27 @@ setup_logging("INFO", None)
 
 from .webSiteLoad import WebSiteLoad
 
-env = Environment(user_classes=[WebSiteLoad])
+def main():
+    env = Environment(user_classes=[WebSiteLoad])
 
-env.create_local_runner()
-if sys.argv[0] == "web-site-load":
-    env.create_web_ui()
-    print("Open your browser to http://localhost:8089/ to configure/run")
-    try:
-        env.web_ui.greenlet.join()
-    except KeyboardInterrupt:
-        env.web_ui.stop()
-        print("Terminating")
+    env.create_local_runner()
+    if sys.argv[0] == "web-site-load":
+        env.create_web_ui()
+        print("Open your browser to http://localhost:8089/ to configure/run")
+        try:
+            env.web_ui.greenlet.join()
+        except KeyboardInterrupt:
+            env.web_ui.stop()
+            print("Terminating")
 
-if sys.argv[0] == "web-site-run":
-    gevent.spawn(stats_printer(env.stats))
-    print("Starting 20 users, 10/s")
-    env.runner.start(20, spawn_rate=10)
-    print("Wait 1 minute and quit...")
-    gevent.spawn_later(60, lambda: env.runner.quit())
-    try:
-        env.runner.greenlet.join()
-    except KeyboardInterrupt:
-        print("Terminating")
+    if sys.argv[0] == "web-site-run":
+        gevent.spawn(stats_printer(env.stats))
+        print("Starting 20 users, 10/s")
+        env.runner.start(20, spawn_rate=10)
+        print("Wait 1 minute and quit...")
+        gevent.spawn_later(60, lambda: env.runner.quit())
+        try:
+            env.runner.greenlet.join()
+        except KeyboardInterrupt:
+            print("Terminating")
 
